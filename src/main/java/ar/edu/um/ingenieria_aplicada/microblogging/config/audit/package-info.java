38ece82b7fc90a44562ea18141baba60616dbea3
aarrnnaall <aarrnnaall@gmail.com>
2018-11-06 11:23:59 -0300

    sin entidades

---
+++ src/main/java/ar/edu/um/ingenieria_aplicada/microblogging/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package ar.edu.um.ingenieria_aplicada.microblogging.config.audit;
---
+++ src/main/java/ar/edu/um/ingenieria_aplicada/microblogging/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package ar.edu.um.ingenieria_aplicada.microblogging.web.rest;
