458b6f89231f18531c9d9aa8e48e2534ac9b8680
aarrnnaall <aarrnnaall@gmail.com>
2018-11-07 10:04:20 -0300

    limpiando

--- src/main/webapp/app/entities/publication/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './publication.service';
-export * from './publication-update.component';
-export * from './publication-delete-dialog.component';
-export * from './publication-detail.component';
-export * from './publication.component';
-export * from './publication.route';
